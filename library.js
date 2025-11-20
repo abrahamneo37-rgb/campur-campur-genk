f6c616b003aa013ae713a8883a9b6381293e77c7
KrispyPasta <abrahamneo37@gmail.com>
2025-11-20 14:50:18 +0700

    penambahan background dan tombol library

---
+++ library.js
@@ -0,0 +1,45 @@
+const listZat = [
+  { id: "h2o", nama: "H₂O (Air)" },
+  { id: "h2o2", nama: "H₂O₂ (Hidrogen Peroksida)" },
+  { id: "garam", nama: "NaCl (Garam Dapur)" },
+  { id: "co2", nama: "CO₂ (Karbon Dioksida)" },
+  { id: "co", nama: "CO (Karbon Monoksida)" },
+  { id: "gula", nama: "C6H12O6 (Gula)" },
+  { id: "h2co3", nama: "H2CO3 (Asam Karbonat)" },
+  { id: "etanol", nama: "C2H5OH (Etanol)" },
+  { id: "naoh", nama: "NaOH (Natrium Hidroksida)" },
+  { id: "naocl", nama: "NaOCL (Natrium Hipoklorit)" },
+  { id: "na2co3", nama: "Na2CO3 (Natrium Karbonat)" },
+  { id: "chcl3", nama: "CHCl3 (Kloroform)" },
+  { id: "cocl2", nama: "COCl2 (Fosgen)" },
+];
+
+// panel DOM
+const listPanel = document.getElementById("library-list");
+const detailPanel = document.getElementById("library-detail");
+
+// Buat tombol list kiri
+listZat.forEach(item => {
+  const btn = document.createElement("button");
+  btn.className = "lib-item";
+  btn.textContent = item.nama;
+
+ btn.addEventListener("click", () => {
+  muatDetail(item.id, item.nama);
+});
+
+  listPanel.appendChild(btn);
+});
+
+// Load detail JSON ke panel kanan
+function muatDetail(id, nama) {
+  fetch(`funFactCard/${id}.json`)
+    .then(res => res.json())
+    .then(data => {
+      detailPanel.innerHTML = `
+        <h2>${nama}</h2>
+        ${data.img ? `<img src="${data.img}" class="lib-img">` : ""}
+        <p>${data.funfact}</p>
+      `;
+    });
+}
